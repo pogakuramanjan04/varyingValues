466a7385845194d1cbbced82525da712e364cb10
Pogaku Ram <125488490+pogakuramanjan04@users.noreply.github.com>
2023-02-21 17:35:59 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./styles.css";
+import { useState } from "react";
+export default function App() {
+  const [v, uv] = useState(0);
+  const [v1, uv1] = useState(0);
+  function e() {
+    let a = parseInt(event.target.value);
+    uv(a);
+
+    let b = a + 10;
+    uv1(b);
+  }
+  function e1() {
+    let a1 = parseInt(event.target.value);
+    uv1(a1);
+    uv(a1 - 10);
+  }
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <input onChange={e} value={v} />
+      <h1>Total salary {v}</h1>
+      <input onChange={e1} value={v1} />
+      <h1>inhand salary {v1}</h1>
+    </div>
+  );
+}
